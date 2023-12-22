498ad7f87827927a7627e9630ea95e50cc30c681
sulochan <sulochankhadka25@gmail.com>
2023-12-23 00:53:52 +0530

    resolved-bugs

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,80 +1,20 @@
-// import React from 'react';
-// import './services.css';
-// const Services = () => {
-//   return (
-//     <div className='service-container'>
-//       <div className='static-part'>
-//         <div className='static-title'>Our Services</div>
-//         <div className='static-desc'>
-//           We help enterprises transform their innovative ideas into successful
-//           products from scratch.
-//         </div>
-//         <div className='static-desc'>
-//           {' '}
-//           From custom product and web development to migrating legacy systems to
-//           modern and efficient Jamstack architecture, we take care of the
-//           technical complexities, so your team can focus on what you do best.
-//         </div>
-//       </div>
-//       <div className='scroll-part'>
-//         <div className='scroll-container'>
-//           <div>Yes</div>
-//         </div>
-//         <div className='scroll-container'>
-//           <div>Yes</div>
-//         </div>
-//         <div className='scroll-container'>
-//           <div>Yes</div>
-//         </div>
-//         <div className='scroll-container'>
-//           <div>Yes</div>
-//         </div>
-//         <div className='scroll-container'>
-//           <div>Yes</div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Services;
-
-import React, { useRef, useEffect } from 'react';
+import React, { useState } from 'react';
+import video1 from '../assets/videos/production_id_4440931 (1080p).mp4';
+import { FaArrowRight } from 'react-icons/fa';
 import './services.css';
-
 const Services = () => {
-  const scrollPartRef = useRef(null);
+  const [fix, setFix] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollPart = scrollPartRef.current;
-      const staticPart = scrollPart.previousElementSibling;
+  const fixSidebar = () => {
+    if (window.scrollY >= 1550 && window.scrollY <= 2530) {
+      setFix(true);
+    } else setFix(false);
+  };
 
-      if (
-        scrollPart.scrollTop <
-        scrollPart.scrollHeight - scrollPart.clientHeight
-      ) {
-        staticPart.style.position = 'fixed';
-        staticPart.style.top = '0';
-      } else {
-        staticPart.style.position = 'unset';
-      }
-    };
-
-    // Capture current value for consistent reference:
-    let currentScrollPart = scrollPartRef.current; // Capture before attaching listener
-
-    currentScrollPart.addEventListener('scroll', handleScroll);
-
-    return () => {
-      currentScrollPart.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
+  window.addEventListener('scroll', fixSidebar);
   return (
     <div className='service-container'>
-      <div className='static-part'>
-        {/* Static content here */}
+      <div className={fix ? 'static-part fix' : 'static-part'}>
         <div className='static-title'>Our Services</div>
         <div className='static-desc'>
           We help enterprises transform their innovative ideas into successful
@@ -87,22 +27,50 @@ const Services = () => {
           technical complexities, so your team can focus on what you do best.
         </div>
       </div>
-      <div className='scroll-part' ref={scrollPartRef}>
-        {/* Scrollable content here */}
+      <div className='scroll-part'>
         <div className='scroll-container'>
-          <div>Yes</div>
+          <div className='card-title'>This is a dummy video </div>
+          <video autoPlay muted loop className='card-video'>
+            <source src={video1} type='video/mp4' />
+            Your browser does not support the video tag.
+          </video>
+          <div className='card-readmore-container'>
+            <div className='card-readmore'>Read More</div>
+            <FaArrowRight />
+          </div>
         </div>
         <div className='scroll-container'>
-          <div>Yes</div>
+          <div className='card-title'>This is a dummy video </div>
+          <video autoPlay muted loop className='card-video'>
+            <source src={video1} type='video/mp4' />
+            Your browser does not support the video tag.
+          </video>
+          <div className='card-readmore-container'>
+            <div className='card-readmore'>Read More</div>
+            <FaArrowRight />
+          </div>
         </div>
         <div className='scroll-container'>
-          <div>Yes</div>
+          <div className='card-title'>This is a dummy video </div>
+          <video autoPlay muted loop className='card-video'>
+            <source src={video1} type='video/mp4' />
+            Your browser does not support the video tag.
+          </video>
+          <div className='card-readmore-container'>
+            <div className='card-readmore'>Read More</div>
+            <FaArrowRight />
+          </div>
         </div>
         <div className='scroll-container'>
-          <div>Yes</div>
-        </div>
-        <div className='scroll-container'>
-          <div>Yes</div>
+          <div className='card-title'>This is a dummy video </div>
+          <video autoPlay muted loop className='card-video'>
+            <source src={video1} type='video/mp4' />
+            Your browser does not support the video tag.
+          </video>
+          <div className='card-readmore-container'>
+            <div className='card-readmore'>Read More</div>
+            <FaArrowRight />
+          </div>
         </div>
       </div>
     </div>
